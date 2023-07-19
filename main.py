a9297913bb92a7d633ba5d1a579d64dd58ff6552
Mariano <rodmariano13@gmail.com>
2023-07-19 11:53:54 -0300

    add create item endpoints

--- main.py
+++ main.py
@@ -1,25 +1,27 @@
 """main"""
 from enum import Enum
+from typing import Any
 
 from fastapi import FastAPI
+from pydantic import BaseModel
 
 app = FastAPI()
 
 
 @app.get("/")
-async def root():
+async def root() -> dict[str, str]:
     """root function"""
     return {"message": "Hello World"}
 
 
 @app.get("/items/1/{item_id}")
-async def read_item1(item_id):
+async def read_item1(item_id: str) -> dict[str, str]:
     """read_item1 function"""
     return {"item_id": item_id}  # item_id: str
 
 
 @app.get("/items/2/{item_id}")
-async def read_item2(item_id: int):
+async def read_item2(item_id: int) -> dict[str, int]:
     """read_item2 function"""
     return {"item_id": item_id}  # item_id: int
 
@@ -27,25 +29,25 @@ async def read_item2(item_id: int):
 class ModelName(str, Enum):
     """ModelName enum class"""
 
-    alexnet = "alexnet"
-    resnet = "resnet"
-    lenet = "lenet"
+    ALEXNET = "ALEXNET"
+    RESNET = "RESNET"
+    LENET = "LENET"
 
 
 @app.get("/models/{model_name}")
-async def get_model(model_name: ModelName):
+async def get_model(model_name: ModelName) -> dict[str, str]:
     """get_model function"""
-    if model_name is ModelName.alexnet:
+    if model_name is ModelName.ALEXNET:
         return {"model_name": model_name, "message": "Deep Learning FTW!"}
 
-    if model_name.value == "lenet":
+    if model_name.value == "LENET":
         return {"model_name": model_name, "message": "LeCNN all the images"}
 
     return {"model_name": model_name, "message": "Have some residuals"}
 
 
 @app.get("/files/{file_path:path}")
-async def read_file(file_path: str):
+async def read_file(file_path: str) -> dict[str, str]:
     """read_file function"""
     return {"file_path": file_path}
 
@@ -54,14 +56,15 @@ fake_items_db = [{"item_name": "Foo"}, {"item_name": "Bar"}, {"item_name": "Baz"
 
 
 @app.get("/items/")
-async def read_item3(skip: int = 0, limit: int = 10):
+async def read_item3(skip: int = 0, limit: int = 10) -> list[dict[str, str]]:
     """read_item3 function"""
     # http://127.0.0.1:8000/items/?skip=0&limit=10
     return fake_items_db[skip : skip + limit]
 
 
 @app.get("/items/{item_id}")
-async def read_item4(item_id: str, q: str | None = None):
+# pylint: disable=invalid-name
+async def read_item4(item_id: str, q: str | None = None) -> dict[str, str]:
     """read_item4 function"""
     if q:
         return {"item_id": item_id, "q": q}
@@ -69,7 +72,10 @@ async def read_item4(item_id: str, q: str | None = None):
 
 
 @app.get("/items/{item_id}")
-async def read_item5(item_id: str, q: str | None = None, short: bool = False):
+# pylint: disable=invalid-name
+async def read_item5(
+    item_id: str, q: str | None = None, short: bool = False
+) -> dict[str, str]:
     """read_item5 function"""
     # http://127.0.0.1:8000/items/foo?short=1
     # http://127.0.0.1:8000/items/foo?short=True
@@ -82,3 +88,41 @@ async def read_item5(item_id: str, q: str | None = None, short: bool = False):
             {"description": "This is an amazing item that has a long description"}
         )
     return item
+
+
+class Item(BaseModel):
+    """Item model"""
+
+    name: str
+    description: str | None = None
+    price: float
+    tax: float | None = None
+
+
+@app.post("/items/")
+async def create_item1(item: Item) -> Item:
+    """create_item1 function"""
+    # item.price + item.name
+    return item
+
+
+@app.post("/items/")
+async def create_item2(item: Item) -> dict[str, Any]:
+    """create_item2 function"""
+    item_dict = item.dict()
+    if item.tax:
+        price_with_tax = item.price + item.tax
+        item_dict.update({"price_with_tax": price_with_tax})
+    return item_dict
+
+
+@app.put("/items/{item_id}")
+# pylint: disable=invalid-name
+async def create_item3(
+    item_id: int, item: Item, q: str | None = None
+) -> dict[str, Any]:
+    """create_item2 function"""
+    result = {"item_id": item_id, **item.dict()}
+    if q:
+        result.update({"q": q})
+    return result
